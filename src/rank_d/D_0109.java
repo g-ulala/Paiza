63eedc5e392b1e14a7dde31c2bbf3c7b873a2890
g-ulala <gradriel13@yahoo.co.jp>
2023-10-27 00:01:22 +0900

    更新

--- src/rank_d/D_0109.java
+++ src/rank_d/D_0109.java
@@ -5,16 +5,13 @@ import java.util.Scanner;
 
 public class D_0109 {
 
-	public D_0109() {
-		// TODO 自動生成されたコンストラクター・スタブ
-	}
+	public D_0109() { }
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print(sameMMDD(sc));
+		System.out.println(sameMMDD(sc));
         sc.close();
 	}
-	
     // 月と日の比較
     private static String sameMMDD(Scanner aSc) {
 
@@ -28,65 +25,47 @@ public class D_0109 {
         	LocalDate.now()
         			.withMonth(inMonth)
         			.withDayOfMonth(inDate);
+
         } catch (Exception e) {
             e.printStackTrace();
-            throw e;
+            return "No";
         }
         return compareResult(inMonth, inDate);
     }
     
     // 月と日の比較結果を返却
     private static String compareResult(int aMonth, int aDate) {
-    	
-    	String result = null;
+
     	String strMonth = String.valueOf(aMonth);
     	String strDate = String.valueOf(aDate);
-    	
-    	int monthTensPlace = 0;
-    	int monthOnesPlace = 0;
-    	int dateTensPlace = 0;
-    	int dateOnesPlace = 0;
 
-    	monthOnesPlace = aMonth % 10;
+    	int[] checkYm = new int[4];
+    	boolean blCheck = false;
+
+    	checkYm[2] = aMonth % 10;			// 月2桁目
     	if (2 == strMonth.length()) {
-    		monthTensPlace = aMonth / 10;
+    		checkYm[3] = aMonth / 10;		// 月1桁目
     	}
 
-    	dateOnesPlace = aDate % 10;
+    	checkYm[0] = aDate % 10; 			// 日1桁目
     	if (2 == strDate.length()) {
-    		dateTensPlace = aDate / 10;
+    		checkYm[1] = aDate / 10;		// 日2桁目
     	}
 
-    	if (0 != dateOnesPlace && 0 != monthOnesPlace) {	// 日付1の位は必ず0以外
-    		if (dateOnesPlace == monthOnesPlace) {			// 月と日の1の位ゾロ目合わせ
-    			if (0 == monthTensPlace) {					// 月の10の位が0時(1の位だけで判定)
-    				if (0 == dateTensPlace) {				// 日の10の位なし
-    					result = "Yes";
-    				} else if (dateTensPlace == dateOnesPlace) {	// 日の1と10の位比較
-    					result = "Yes";
-    				}
-    			} else {									// 月の10の位が0以外
-    				if (0 == dateTensPlace) {
-    					if (monthTensPlace == monthOnesPlace) {		// 月の1の位と10の位が同じ
-    						result = "Yes";
-    					} else {
-    						result = "No";
-    					}
-    				} else {
-    					if (dateTensPlace == monthTensPlace && dateTensPlace == monthOnesPlace) { // 日と月のゾロ目チェック
-    						result = "Yes";
-    					} else {
-    						result = "No";
-    					}
+    	for (int i = 1; i < checkYm.length; i++) {
+    		if (0 == checkYm[0] || 0 == checkYm[2]) {	// 日と月の1桁目が0はあり得ない
+    			blCheck = false;
+    			break;
+    		} else {
+    			if (0 == checkYm[i]) {					// 日と月の2桁目が0の時は無視
+    				continue;
+    			} else {
+    				if (checkYm[0] == checkYm[i]) {		// 日の1桁目と同一
+    					blCheck = true;
     				}
     			}
-    		} else {
-    			result = "No";
     		}
-    	} else {
-    		result = "No";
     	}
-
-    	return result;
+    	return blCheck ? "Yes" : "No";
     }
 }
